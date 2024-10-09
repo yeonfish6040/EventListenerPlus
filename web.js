33446fb91e306d644dff770aaee09e4cf7dc3897
Yeonjun Lee <yeonfish6040@gmail.com>
2024-10-09 21:48:27 +0900

    init

---
+++ web.js
@@ -0,0 +1,16 @@
+window.addEventsListener = (events) => {
+    Object.keys(events).forEach(eventName => {
+        Array.from(events[eventName]).forEach(eventHandler => {
+            let animation = eventHandler.animation;
+            if (eventHandler.hasOwnProperty("nomobile")) animation = (...args) => {if (window.innerWidth <= 720) return; eventHandler.animation(...args);};
+            if (eventHandler.selector === "document") {
+                document.addEventListener(eventName, (e) => animation(e, document));
+                return;
+            }
+            if (eventHandler.hasOwnProperty("all"))
+                document.querySelectorAll(eventHandler.selector).forEach(element => element.addEventListener(eventName, (e) => animation(e, element)));
+            else
+                document.querySelector(eventHandler.selector).addEventListener(eventName, (e) => animation(e, document.querySelector(eventHandler.selector)));
+        });
+    });
+}
